7667d78235110d9869e9e4cecb58cee17a7b509e
Mike <mike.zhang@activenetwork.com>
2017-04-06 08:22:36 +0800

    add

--- static/cart.js
+++ static/cart.js
@@ -1,15 +1,49 @@
 var vm = new Vue({
     el: "#app",
     data:{
-       title: "Hello Vue"
+       totalMoney : 0,
+       productList:[]
     },
     filters:{
-
+       formatMoney: function(value){
+          return "$ " + value.toFixed(2);
+       }
     },
     mounted:function(){
-
+       this.$nextTick(function(){
+           this.cartView();
+       });       
     },
     methods:{
-        
+        cartView: function(){
+            let _this = this;
+            this.$http.get("/static/data/cartData.json").then(res=>{
+                this.productList = res.data.result.list;
+                this.totalMoney = res.data.result.totalMoney;
+            });
+        },
+        changeMoney: function(product, way){
+            if(way > 0){
+                product.productQuentity++;
+            }else{
+                if(product.productQuentity < 2){
+                    product.productQuentity = 1;
+                }else{
+                    product.productQuentity--;
+                }
+            }
+        },
+        selectedProduct: function(product){
+            if(typeof product.checked == "undefined" ){
+                //Vue.set(item, "checked", true);
+                this.$set(product, "checked", true);
+            }else{
+                product.checked = !product.checked;
+            }
+        }
     }
+});
+
+Vue.filter("money", function(value, type){
+    return "$ " + value.toFixed(2) +" "+ type;
 });
